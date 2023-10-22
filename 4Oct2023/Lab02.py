2a14377ab10bfdd463d4ad3e631fca1f169294cd
SP2205 <118822487+SP2205@users.noreply.github.com>
2023-10-22 22:03:01 +0530

    Add files via upload

---
+++ 4Oct2023/Lab02.py
@@ -0,0 +1,22 @@
+
+#Data types
+
+x=5
+print(type(x))
+
+Name= "Supriya"
+print(type(Name))
+
+num=12
+print(type(num))
+
+pi=3.14
+print((type(pi)))
+
+Testboolean = False
+print((type(Testboolean)))
+
+complexnum=3+5j
+print(type(complexnum))
+
+
---
+++ 4Oct2023/Task-4Oct2023.py
@@ -0,0 +1,18 @@
+#How to take user input
+
+Name = input("Enter your name")
+print(Name)
+
+#Task2
+FirstName= input("Enter First Name :")
+LastFName= input("Enter Last Name :")
+
+print(FirstName,LastFName,sep='-', end='\n')
+#sep used to seprate name using- ,
+# \t is used to give space
+# \n used for new line
+
+# Task 3
+Message = input("Enter your name :")
+print("You are Welcome",Message,"to the python course")
+
